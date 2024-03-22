10bb6ee1e0a1d11950d0550dc4130a4b3f84c1d0
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-03-22 13:52:31 +0100

    Add resposnive design for trivia page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,62 @@
 import { NavLink } from "react-router-dom";
 import styles from "../styles/NavBar.module.scss";
 import Logo from "./Logo";
+import { useEffect, useRef, useState } from "react";
 
 export default function NavBar() {
+  const [activeNav, setActiveNav] = useState(false);
+  const navBar = useRef(null);
+  const phoneNavContainer = useRef(null);
+  const phoneNavSlash = useRef(null);
+  const phoneNavLinks = useRef(null);
+
+  function handleActiveNav() {
+    setActiveNav(() => !activeNav);
+  }
+
+  useEffect(
+    function () {
+      if (activeNav) {
+        navBar.current.classList.add("navActive");
+        phoneNavContainer.current.classList.add("navActive");
+        phoneNavSlash.current.classList.add("navActive");
+        phoneNavLinks.current.classList.add("navActive");
+        return;
+      }
+      navBar.current.classList.remove("navActive");
+      phoneNavContainer.current.classList.remove("navActive");
+      phoneNavSlash.current.classList.remove("navActive");
+      phoneNavLinks.current.classList.remove("navActive");
+    },
+    [activeNav]
+  );
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbar} ref={navBar}>
       <Logo />
-      <ul>
+      <ul className={styles.navbarLinks}>
+        <li>
+          <NavLink to="/trivia">Trivia</NavLink>
+        </li>
+        <li>
+          <NavLink to="/jokes">Jokes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/advice">Advice</NavLink>
+        </li>
+      </ul>
+
+      <div
+        className={styles.navSlash}
+        ref={phoneNavSlash}
+        onClick={() => handleActiveNav()}
+      ></div>
+      <div
+        className={styles.phoneNavContainer}
+        onClick={() => handleActiveNav()}
+        ref={phoneNavContainer}
+      ></div>
+      <ul className={styles.phoneNavLinks} ref={phoneNavLinks}>
         <li>
           <NavLink to="/trivia">Trivia</NavLink>
         </li>
